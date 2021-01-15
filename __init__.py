80b7ead150d541e4016cd0fad924ee30afe02859
Jairo Martinez Antonio <jmartinez@aeromexico.com>
2021-01-15 10:35:51 -0600

    Add on_lexer event

--- __init__.py
+++ __init__.py
@@ -10,31 +10,14 @@ if os.path.isfile(fn_config):
     with open(fn_config, 'r') as f:
         colormap = json.load(f)
 
+
 class Command:
 
     def on_open(self, ed_self):
+        color_tab(ed_self)
 
-        filename = ed_self.get_filename()
-        lexer = ed_self.get_prop(PROP_LEXER_FILE)
-        val = None
-
-        global colormap
-        for key in colormap:
-            if key[0] == '.':
-                if filename.endswith(key):
-                    val = colormap[key]
-                    break
-            elif key.lower() == lexer.lower():
-                val = colormap[key]
-                break
-
-        if val is not None:
-            try:
-                n = html_color_to_int(val)
-                ed_self.set_prop(PROP_TAB_COLOR, n)
-            except:
-                print('NOTE: Tab Colors: bad color string in config "%s"'%val)
-                pass
+    def on_lexer(self, ed_self):
+        color_tab(ed_self)
 
     def config(self):
 
@@ -42,3 +25,27 @@ class Command:
             with open(fn_config, 'w') as f:
                 f.write('{}\n')
         file_open(fn_config)
+
+
+def color_tab(ed_self):
+    filename = ed_self.get_filename()
+    lexer = ed_self.get_prop(PROP_LEXER_FILE)
+    val = None
+
+    global colormap
+    for key in colormap:
+        if key[0] == '.':
+            if filename.endswith(key):
+                val = colormap[key]
+                break
+        elif key.lower() == lexer.lower():
+            val = colormap[key]
+            break
+
+    if val is not None:
+        try:
+            n = html_color_to_int(val)
+            ed_self.set_prop(PROP_TAB_COLOR, n)
+        except:
+            print('NOTE: Tab Colors: bad color string in config "%s"' % val)
+            pass
